2f84fc9535bb1dfb02ad551b0127d7368917023c
= <=>
2022-05-31 13:58:12 +0100

    add network

---
+++ pong_dqn_rl/dueling_qn.py
@@ -0,0 +1,62 @@
+"""Defines the Neural Network"""
+from typing import Tuple
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DuelCNN(nn.Module):
+    """
+    CNN with Duel Algo.
+    """
+
+    def __init__(self, h: int, w: int, output_size: int):
+        super(DuelCNN, self).__init__()
+
+        # Representation layers
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        convw, convh = self.conv2d_size_calc(w, h, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        convw, convh = self.conv2d_size_calc(convw, convh, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        convw, convh = self.conv2d_size_calc(convw, convh, kernel_size=3, stride=1)
+        linear_input_size = convw * convh * 64  # Last conv layer's out sizes
+
+        # Action layer
+        self.Alinear1 = nn.Linear(in_features=linear_input_size, out_features=128)
+        self.Alrelu = nn.LeakyReLU()  # Linear 1 activation funct
+        self.Alinear2 = nn.Linear(in_features=128, out_features=output_size)
+
+        # State Value layer
+        self.Vlinear1 = nn.Linear(in_features=linear_input_size, out_features=128)
+        self.Vlrelu = nn.LeakyReLU()  # Linear 1 activation funct
+        self.Vlinear2 = nn.Linear(in_features=128, out_features=1)  # Only 1 node
+
+    def conv2d_size_calc(
+        self, w: int, h: int, kernel_size: int = 5, stride: int = 2
+    ) -> Tuple[int, int]:
+        """
+        Calcs conv layers output image sizes
+        """
+        next_w = (w - (kernel_size - 1) - 1) // stride + 1
+        next_h = (h - (kernel_size - 1) - 1) // stride + 1
+        return next_w, next_h
+
+    def forward(self, x) -> float:
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        x = x.view(x.size(0), -1)  # Flatten every batch
+
+        Ax = self.Alrelu(self.Alinear1(x))
+        Ax = self.Alinear2(Ax)  # No activation on last layer
+
+        Vx = self.Vlrelu(self.Vlinear1(x))
+        Vx = self.Vlinear2(Vx)  # No activation on last layer
+
+        q = Vx + (Ax - Ax.mean())
+
+        return q
